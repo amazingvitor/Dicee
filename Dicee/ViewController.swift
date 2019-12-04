c42450ae5acb93a5f7aea0107e9097a75662317d
Vitor Hugo S <vitor.h8@icloud.com>
2019-12-04 12:43:16 -0300

    Final Project

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    // Dice Images Outlets
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Dice Images when launch
+        leftDice.image = #imageLiteral(resourceName: "DiceFive")
+        rightDice.image = #imageLiteral(resourceName: "DiceTwo")
     }
-
-
+    
+    // Roll Button Action
+    @IBAction func rollButton(_ sender: Any) {
+        
+        let imageArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        leftDice.image = imageArray[Int.random(in: 1...5)]
+        rightDice.image = imageArray[Int.random(in: 1...5)]
+    }
+    
+    
 }
 
